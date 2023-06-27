b0edf0766b543182a75a0f1274caa917e781e945
Maxsos133 <max.pavlenko133@gmail.com>
2023-06-27 13:49:21 -0500

    fix

--- public/script.js
+++ public/script.js
@@ -34,30 +34,30 @@ async function checkLogin() {
 }
 
 
-async function checkUserIdIf () {
-  if (localStorage.getItem(`userId`) === null) {
-    await setTimeout(async () => {
-      await checkLogin();
-      loginDiv.innerHTML = loginButton;
-      if(localStorage.getItem(`userId`)) {
-      await setTimeout(async () => {
-        loginDiv.innerHTML = logoffButton
-          let logoutBtn = document.querySelector(`#logoutBtn`)
-          logoutBtn.addEventListener(`click`, function() {
-            localStorage.removeItem('userId')
-          })
+async function checkUserIdIf() {
+  if (localStorage.getItem('userId') === null) {
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    await checkLogin();
+    loginDiv.innerHTML = loginButton;
+    if (localStorage.getItem('userId')) {
+      setTimeout(() => {
+        loginDiv.innerHTML = logoffButton;
+        let logoutBtn = document.querySelector('#logoutBtn');
+        logoutBtn.addEventListener('click', function() {
+          localStorage.removeItem('userId');
+        });
       }, 1500);
     }
-    }, 3000);
-  } else if(localStorage.getItem(`userId`)) {
-    loginDiv.innerHTML = logoffButton
-    let logoutBtn = document.querySelector(`#logoutBtn`)
-    logoutBtn.addEventListener(`click`, function() {
-      localStorage.removeItem('userId')
-    })
+  } else if (localStorage.getItem('userId')) {
+    loginDiv.innerHTML = logoffButton;
+    let logoutBtn = document.querySelector('#logoutBtn');
+    logoutBtn.addEventListener('click', function() {
+      localStorage.removeItem('userId');
+    });
   }
 }
 
-checkUserIdIf()
+checkUserIdIf();
+
 
 
