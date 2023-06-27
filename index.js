43bace03bde3139e39c730853fcd3182b0ab6668
Maxsos133 <max.pavlenko133@gmail.com>
2023-06-27 10:36:00 -0500

    fix

--- index.js
+++ index.js
@@ -52,11 +52,8 @@ app.get('/', (req, res) => {
 app.use(`/`, AppRouter)
 
 app.get('/check-login', (req, res) => {
-  if (req.isAuthenticated()) {
     res.json(req.user)
-  } else {
-    res.status(401).json({ error: 'User is not logged in' })
-  }
+  
 })
 
 const { User } = require('./models')
