104009ba8adf023554cebb0bc0ef353c0f0a886e
Maxsos133 <max.pavlenko133@gmail.com>
2023-06-27 11:22:19 -0500

    fix

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ require(`./db/passport`)
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://tarkov-test.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(cors())
 app.use(express.json())
