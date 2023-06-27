cafe5c5f99fca4d0e914df18de46a8b1282f06fe
Maxsos133 <max.pavlenko133@gmail.com>
2023-06-27 12:00:49 -0500

    fix

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ app.use(logger(`dev`))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, `./`)))
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, `public`)))
 
 
 
